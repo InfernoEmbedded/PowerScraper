5f6f5489d3325af102549d29e932bd3220092f06
Alastair D'Silva <alastair@d-silva.org>
2018-06-09 14:21:46 +1000

    Remove unicode copyright symbol

--- solax_scraper.py
+++ solax_scraper.py
@@ -7,7 +7,7 @@
 #   cp config-example.toml config.toml
 #   vi config.toml 
 #
-# Copyright ©2018 Inferno Embedded   http://infernoembedded.com
+# Copyright (c)2018 Inferno Embedded   http://infernoembedded.com
 # 
 # This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
